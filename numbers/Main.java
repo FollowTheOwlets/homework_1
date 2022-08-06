020e1b674c46d9327bea61025d6f8cae74664302
Lev <97794266+FollowTheOwlets@users.noreply.github.com>
2022-08-06 14:31:26 +0300

    Update Main.java

--- numbers/Main.java
+++ numbers/Main.java
@@ -1,1 +1,24 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int num : intList) {
+            if (num > 0 && num % 2 == 0) {
+                resultList.add(num);
+            }
+        }
+
+        resultList.sort(Comparator.naturalOrder());
+        resultList.forEach(System.out::println);
+    }
+}
+
+
